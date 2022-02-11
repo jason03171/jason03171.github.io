1baefa135cd8d53ac5e0c77b448fe77e8d12db23
jason <jason0317@kimo.com>
2022-02-11 10:18:11 +0800

    add experience-section with vue

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
-const nav = document.querySelector('nav');
-const navHeight = nav.offsetHeight;
+let data = {
+  experience : [
+    {
+      workyear : "2018/1~2019/1",
+      company : "睿傳資訊股份有限公司",
+      jobtitle: "資訊助理人員",
+      content : ["軟體測試","硬體測試","撰寫圖表程式"]
+    },
+    {
+      workyear : "2019/12~2021/2",
+      company : "豐軒科技股份有限公司 ",
+      jobtitle: "助理工程師",
+      content : 
+      [
+        "產品文件撰寫(產品功能FA、產品畫面FA、產品說明)",
+        "測試API",
+        "撰寫自動化測試程式",
+        "處理客服人員及業務助理回報問題",
+        "處理公司產品方向及營運方向的工作"
+      ]
+    }
+  ]
+};
 
-function animateNav() {
-  if (window.pageYOffset > navHeight) { return nav.classList.add('black', 'lighten-3', 'shadow') }
-  else nav.classList.remove('black', 'lighten-3', 'shadow')
-}
-
-window.addEventListener('scroll', () => {
-  animateNav()
-})
+let vm = new Vue({
+  el:"#app",
+  data:data,
+  mounted() {
+    window.addEventListener('scroll', () => {
+      const nav = this.$el.querySelector("nav");
+      const navHeight = nav.offsetHeight;
+      if (window.pageYOffset > navHeight) { 
+        return nav.classList.add('black', 'lighten-3', 'shadow') 
+      }
+      else nav.classList.remove('black', 'lighten-3', 'shadow')
+    })
+  },
+});
